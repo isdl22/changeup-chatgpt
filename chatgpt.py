3f701e1d09bd177494a301efb9303e257d86a578
isdl22 <34301347+isdl22@users.noreply.github.com>
2023-11-06 22:10:27 +0900

    Update chatgpt.py

--- chatgpt.py
+++ chatgpt.py
@@ -1,11 +1,3 @@
-from dotenv import load_dotenv
-load_dotenv()
-import openai
-import os
-openai.api_key = os.environ.get("OPENAI_API_KEY")
-import time
-from langchain.chat_models import ChatOpenAI
-from langchain.memory import ConversationBufferMemory
 from langchain.prompts import (
     ChatPromptTemplate,
     MessagesPlaceholder,
@@ -13,9 +5,17 @@ from langchain.prompts import (
     HumanMessagePromptTemplate,
 )
 from langchain.chains import LLMChain
+from langchain.memory import ConversationBufferMemory
+from langchain.chat_models import ChatOpenAI
 import streamlit as st
-st.title("체인지업 ChatGPT")
+import time
 
+import dotenv
+dotenv.load_dotenv()
+
+st.title("ChatGPT")
+
+# Set a default model
 if "openai_model" not in st.session_state:
     st.session_state["openai_model"] = "gpt-3.5-turbo"
 
@@ -26,7 +26,7 @@ for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
-if prompt := st.chat_input("여기에 물어보세요."):
+if prompt := st.chat_input("What is up?"):
     st.session_state.messages.append({"role": "user", "content": prompt})
     with st.chat_message("user"):
         st.markdown(prompt)
@@ -35,10 +35,10 @@ if prompt := st.chat_input("여기에 물어보세요."):
         message_placeholder = st.empty()
         full_response = ""
         # LLM
-        # llm =  ChatOpenAI()
+        llm = ChatOpenAI()
 
         # Prompt
-        custom_prompt = ChatPromptTemplate(
+        llm_prompt = ChatPromptTemplate(
             messages=[
                 SystemMessagePromptTemplate.from_template(
                     "You are a nice chatbot having a conversation with a human."
@@ -48,16 +48,17 @@ if prompt := st.chat_input("여기에 물어보세요."):
                 HumanMessagePromptTemplate.from_template("{question}"),
             ]
         )
+
         # Notice that we `return_messages=True` to fit into the MessagesPlaceholder
         # Notice that `"chat_history"` aligns with the MessagesPlaceholder name
         memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
-        conversation = LLMChain(llm=ChatOpenAI(), prompt=custom_prompt, verbose=True, memory=memory)
+        conversation = LLMChain(llm=llm, prompt=llm_prompt, verbose=True, memory=memory)
 
-        # Notice that we just pass in the `question` variables - `chat_history` gets populated by memory
         result = conversation({"question": prompt})
         for chunk in result['text'].split():
             full_response += chunk + " "
             time.sleep(0.05)
+
             message_placeholder.markdown(full_response + "▌")
         message_placeholder.markdown(full_response)
     st.session_state.messages.append({"role": "assistant", "content": full_response})
